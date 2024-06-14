9d5021d184c47474f14cb59d659397f126753752
kayson3 <ayomiposiibikunle@gmail.com>
2024-06-14 23:48:07 +0100

    referral done with query params

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1948888459"
+    serviceWorkerVersion: "3453081276"
   }
 });
