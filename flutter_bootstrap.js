756b509ec05052890de18fe7fc7f2f5c9e4ce8eb
kayson3 <ayomiposiibikunle@gmail.com>
2024-06-21 07:15:37 +0100

    sell

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "250869506"
+    serviceWorkerVersion: "3665705609"
   }
 });
