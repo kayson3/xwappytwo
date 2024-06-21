79e94020ff2eea50e1e653968ba7b7dd4938c895
kayson3 <ayomiposiibikunle@gmail.com>
2024-06-21 23:00:31 +0100

    s

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "632607551"
+    serviceWorkerVersion: "3443814896"
   }
 });
